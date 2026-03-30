31e20e129305185b1dce682fa8c09e451192ea65
AidanTsang <aidan.bokbok@gmail.com>
2026-03-30 17:41:56 -0400

    Removed print statement

--- src/training/evaluate.py
+++ src/training/evaluate.py
@@ -147,7 +147,6 @@ def evaluate_classifier(model, test_loader: DataLoader, test_y: torch.Tensor, fe
     for _, y_batch in test_loader:
         all_labels.append(y_batch)
     labels = torch.cat(all_labels)
-    print(preds[:10], labels[:10])
 
     accuracy = (preds == labels).float().mean().item()
     nll = -log_probs.mean().item()
